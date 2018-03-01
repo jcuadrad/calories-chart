6d0b8b53596e58db6a96d19bf974ba7e5c2e4538
Juan Cuadra <jcb8793@gmail.com>
2018-03-01 00:11:38 -0600

    Parse CSV to JSON

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Papa from 'papaparse';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      results: 'Nothing!'
+    }
+    this.parseFiles = this.parseFiles.bind(this);
+    this.showResult = this.showResult.bind(this);
+  }
+
+  showResult(results) {
+    this.setState({
+      results: results
+    });
+  }
+
+  parseFiles() {
+    Papa.parse('https://cdn.glitch.com/794ebe4e-78f8-4c05-8e1d-a4d8ec75f1b6%2FCaloresTest.csv?1519881962478', {
+      download: true,
+      header: true,
+      dynamicTyping: true,
+      complete: (results) => {
+        this.showResult(results);
+        console.log(results);
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div onClick={() => this.parseFiles()}>
+          <h1>PARSE!</h1>
+        </div>
+        <pre>
+          {JSON.stringify(this.state.results, null, 2)}
+        </pre>
       </div>
     );
   }
 }
 
 export default App;
+
